1cc02cad8b7e4014807a2a1bee5e0971c029ed32
Canan Ağbal <agbalcanan8069@gmail.com>
2023-03-14 12:34:05 +0300

    darkMode

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,7 +6,7 @@ const Register = () => {
     <div className="overflow-hidden flex-1 h-screen justify-center items-center bg-[#23242a]">
       <div className={`form-container mt-[5vh] w-[380px] h-[580px]`}>
         <form >
-          
+
           <h2 className="text-red-main text-2xl font-[500] text-center tracking-[0.1em] mb-3">
             Sign Up
           </h2>
@@ -28,7 +28,7 @@ const Register = () => {
               name="floating_text"
               type="text"
               required
-              className="peer"
+              className="peer" // tailwinde özel class. 
               placeholder=" "
               
             />
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,6 @@ module.exports = {
       },
     },
   },
-  darkMode: "class",
+  darkMode: "class", //darkmodu uygulamak için yapıyoruz
   plugins: [require("tw-elements/dist/plugin")],
 };
